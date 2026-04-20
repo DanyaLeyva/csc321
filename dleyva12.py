8fb601b4e1ae39c9528a5a827cad544c313c30f2
danya <danyaleyva@yahoo.com>
2026-04-20 18:40:10 +0000

    adding new lab

---
+++ dleyva12.py
@@ -0,0 +1,27 @@
+# Danya Leyva
+# LAB: 12
+# CSC 321
+
+import math
+
+def calc_area_of_rect(base, height):
+    area = base * height
+    return area
+
+def calc_area_of_circle(radius):
+    area = math.pi * (radius ** 2)
+    return area
+
+def dleyva12():
+    rect_base = float(input("Enter the BASE of the rectangle: "))
+    rect_height = float(input("Enter the HEIGHT of the rectangle: "))
+    
+    rect_area = calc_area_of_rect(rect_base, rect_height)
+    print("The area of the rectangle is:", rect_area)
+
+    radius = float(input("Enter the RADIUS of the circle: "))
+    
+    circle_area = calc_area_of_circle(radius)
+    print("The area of the circle is:", circle_area)
+
+dleyva12()
