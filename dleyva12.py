b24b74a872daebe260bc7c7bf2333e3e7ae433b2
danya <danyaleyva@yahoo.com>
2026-05-14 19:08:40 +0000

    updated lab 12

--- dleyva12.py
+++ dleyva12.py
@@ -12,16 +12,13 @@ def calc_area_of_circle(radius):
     area = math.pi * (radius ** 2)
     return area
 
-def dleyva12():
-    rect_base = float(input("Enter the BASE of the rectangle: "))
-    rect_height = float(input("Enter the HEIGHT of the rectangle: "))
+rect_base = float(input("Enter the BASE of the rectangle: "))
+rect_height = float(input("Enter the HEIGHT of the rectangle: "))
     
-    rect_area = calc_area_of_rect(rect_base, rect_height)
-    print("The area of the rectangle is:", rect_area)
+rect_area = calc_area_of_rect(rect_base, rect_height)
+print("The area of the rectangle is:", rect_area)
 
-    radius = float(input("Enter the RADIUS of the circle: "))
+radius = float(input("Enter the RADIUS of the circle: "))
     
-    circle_area = calc_area_of_circle(radius)
-    print("The area of the circle is:", circle_area)
-
-dleyva12()
+circle_area = calc_area_of_circle(radius)
+print("The area of the circle is:", circle_area)
